5e0e3376f0d8ce6e19e5f93c159bf44f0e11486c
devXamr <devxamr@gmail.com>
2025-09-29 21:47:58 +0530

    feat: persist messageList in localStorage

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,43 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+
+async function fetchLocalMessages() {
+  const allMessages = localStorage.getItem("localMessages");
+
+  if (!allMessages) {
+    return [];
+  } else return JSON.parse(allMessages);
+}
 
 export default function Home() {
   const [message, setMessage] = useState("");
   const [messageList, setMessageList] = useState<any[]>([]);
 
+  useEffect(() => {
+    fetchLocalMessages().then(setMessageList);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("localMessages", JSON.stringify(messageList));
+  }, [messageList]);
+
   function handleMessageSubmission() {
     console.log("Here is the message that was sent", message);
-    setMessageList((prev) => [...prev, message]);
-    setMessage("");
+
+    if (messageList) {
+      setMessageList((prev) => [...prev, message]);
+      setMessage("");
+    }
   }
   return (
     <div className="flex flex-col h-screen">
+      <div>This will be the top bar</div>
       <div className="flex-1 w-[650px] border border-red-500 mx-auto">
-        {messageList.length > 0 &&
+        {messageList &&
+          messageList.length > 0 &&
           messageList.map((eachMessage) => (
             <div
               key={uuidv4()}
