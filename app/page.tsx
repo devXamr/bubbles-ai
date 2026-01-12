6f9ee56b20593d3177fc07509c6b359988e46b6c
devXamr <devxamr@gmail.com>
2026-01-12 16:36:20 +0530

    feat: responsive landing page

--- app/page.tsx
+++ app/page.tsx
@@ -47,36 +47,35 @@ export default function Home() {
     /* dark mode later for this page. */
   }
   return (
-    <div
-      className="font-primary relative before:absolute before:top-0 before:left-0 before:w-full
-     before:h-full before:content-[''] before:opacity-[0.03] before:z-10 before:pointer-events-none
-     before:bg-[url('https://www.ui-layouts.com/noise.gif')]"
-    >
-      <section className="px-3 py-3 font-primary mx-3 my-3 ">
-        <div className="text-xs border w-fit px-1 bg-green-50 border-green-100  py-0.5">
-          Your second brain, powered by AI
-        </div>
-        <div className="text-5xl mt-2">
-          Capture Ideas.
-          <br />
-          <span className="text-green-600">Ask AI Anything.</span>
-          <br /> Take Action.
-        </div>
-        <div className="text-lg mt-4 text-gray-600 ">
-          The only knowledge management tool that feels as natural as texting a
-          friend. Capture your thoughts instantly, and let AI transform them
-          into actionable insights.
-        </div>
-
-        <button className="bg-gradient-to-br text-white font-semibold from-green-300 via-green-500 to-green-600 block w-full py-2 mt-5 rounded-sm ">
-          Start Free Trial
-        </button>
-
-        <button className="block w-full py-2 rounded-sm border border-green-300 text-green-900 bg-green-100 mt-2">
-          Watch Demo
-        </button>
-
+    <div className="font-primary">
+      <section className="px-3 py-3 font-primary mt-3 lg:max-w-[40%]  mx-auto border-x bg-green-50">
         <div>
+          <div className="text-xs border w-fit px-1 bg-green-50 border-green-100  py-0.5">
+            Your second brain, powered by AI
+          </div>
+          <div className="text-5xl mt-2">
+            Capture Ideas.
+            <br />
+            <span className="text-green-600">Ask AI Anything.</span>
+            <br /> Take Action.
+          </div>
+          <div className="text-lg mt-4 text-gray-600 ">
+            The only knowledge management tool that feels as natural as texting
+            a friend. Capture your thoughts instantly, and let AI transform them
+            into actionable insights.
+          </div>
+          <div className="lg:flex gap-4">
+            <button className="bg-gradient-to-br text-white font-semibold from-green-300 via-green-500 to-green-600 block w-full lg:w-[40%] py-2 mt-5 lg:mt-2 rounded-sm ">
+              Start Free Trial
+            </button>
+
+            <button className="block w-full  py-2 rounded-sm border border-green-300 text-green-900 bg-green-100 mt-2 lg:w-[40%]">
+              Watch Demo
+            </button>
+          </div>
+        </div>
+
+        <div className="lg:w-[60%] lg:mx-auto">
           <div className="bg-gray-50 rounded-md border mt-5 px-3 py-2 shadow-md">
             <div className="max-w-[80%] ml-auto mt-3 bg-green-500 rounded-md px-3 text-white font-medium py-2">
               This is an human message here
@@ -99,8 +98,8 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="px-4 py-4">
-        <div className="text-3xl text-gray-900 text-left mt-10">
+      <section className="px-4 py-4 lg:w-[40%] lg:mx-auto lg:border-x lg:pt-14">
+        <div className="text-3xl text-gray-900 text-left mt-10 lg:mt-0">
           Everything you need in{" "}
           <span className="text-green-600">one place.</span>
         </div>
@@ -158,7 +157,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="px-4 py-4">
+      <section className="px-4 py-4 lg:w-[40%] lg:mx-auto lg:border-x lg:bg-green-50">
         <div className="text-3xl text-gray-900 text-left mt-10">
           Simple. Powerful, Intelligent.
         </div>
@@ -208,7 +207,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="px-4 py-4">
+      <section className="px-4 py-4 lg:max-w-[40%] mx-auto lg:border-x">
         <div className="text-3xl text-gray-900 text-left mt-10">
           Trusted by Knowledge Workers Everywhere.
         </div>
@@ -248,21 +247,8 @@ export default function Home() {
         </div>
       </section>
 
-      <div className="relative">
-        <div className="absolute w-full h-full -z-1 bg-green-700">
-          <LightRays
-            raysOrigin="top-center"
-            raysColor="black"
-            raysSpeed={1.5}
-            lightSpread={2}
-            rayLength={1.2}
-            followMouse={true}
-            mouseInfluence={2}
-            noiseAmount={0.4}
-            distortion={0.05}
-            className="custom-rays"
-          />
-        </div>
+      <div className="relative lg:mx-auto lg:w-[42%] lg:shadow-md">
+        <div className="absolute w-full h-full lg:mx-auto -z-1 bg-green-700 lg:rounded-sm"></div>
 
         <div className="px-4 py-4">
           <div className="text-3xl text-gray-100 font-semibold text-left mt-10">
@@ -280,25 +266,29 @@ export default function Home() {
             Watch Demo
           </button>
 
-          <div className="text-lg mt-2 text-gray-700  text-left mb-10">
+          <div className="text-sm mt-2 font-light  text-left mb-10 lg-text-center text-white lg:text-center">
             No credit card required • 14-day free trial • Cancel anytime
           </div>
         </div>
       </div>
-      <div className="py-10 px-5">
+      <div className="py-10 lg:pb-0 lg:max-w-[40%] lg:mx-auto lg:border-x">
         <div>
-          <div>Bubbles</div>
-          <div>
+          <div className="pb-2 px-5 text-xl font-medium">Bubbles AI</div>
+          <div className="flex gap-10 text-sm font-light py-10 px-5">
             <div>
               <div>Resources</div>
               <div>Technology</div>
               <div>Contact Us</div>
             </div>
-            <div>Privacy Policy</div>
-            <div>Terms of Services</div>
+            <div>
+              <div>Privacy Policy</div>
+              <div>Terms of Services</div>
+            </div>
           </div>
         </div>
-        <div>Copyright © 2026, bubbles.ai. All Rights Reserved.</div>
+        <div className="bg-green-100 text-xs font-light py-4 lg:py-2 text-center">
+          Copyright © 2026, bubbles.ai. All Rights Reserved.
+        </div>
       </div>
     </div>
   );
